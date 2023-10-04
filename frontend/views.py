203c52f2ae5587eaf9cbbb8f60f6a17fe5481502
Guilherme Brás <gui.cenasrandom@gmail.com>
2023-10-04 16:06:16 +0100

    first commit

---
+++ frontend/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
+def video1(request):
+    return render(request, 'video1.html')
+
+def video2(request):
+    return render(request, 'video2.html')
+def video3(request):
+    return render(request, 'video3.html')
+    
+def video4(request):
+    return render(request, 'video4.html')
+
+def video5(request):
+    return render(request, 'video5.html')
+
+def video6(request):
+    return render(request, 'video6.html')
+
